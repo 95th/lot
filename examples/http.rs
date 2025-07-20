7baec2fc40ffdf1b5e4f0abea1dcda09703eb835
Gurwinder Singh <vargwin@gmail.com>
2025-07-20 21:05:33 +0530

    Simpler example

--- examples/http.rs
+++ examples/http.rs
@@ -2,7 +2,6 @@ use std::time::Duration;
 
 use anyhow::Result;
 use lot::executor::Executor;
-use lot::scenario::Scenario;
 use reqwest::Client;
 
 #[tokio::main]
@@ -11,32 +10,14 @@ async fn main() {
     executor.add_stage(0, 100, Duration::from_secs(10));
     executor.add_stage(100, 100, Duration::from_secs(10));
     executor.add_stage(100, 0, Duration::from_secs(10));
-    executor.run(CallLocalhost::new()).await;
+
+    let client = Client::new();
+    executor.run(|| call_localhost(client.clone())).await;
 }
 
-struct CallLocalhost {
-    client: Client,
-}
-
-impl CallLocalhost {
-    fn new() -> Self {
-        Self {
-            client: Client::builder()
-                .danger_accept_invalid_certs(true)
-                .build()
-                .unwrap(),
-        }
-    }
-}
-
-impl Scenario for CallLocalhost {
-    fn run(&self) -> impl Future<Output = Result<()>> + Send + 'static {
-        let client = self.client.clone();
-        async move {
-            let response = client.get("https://localhost:8080").send().await?;
-            let response = response.error_for_status()?;
-            response.bytes().await?;
-            Ok(())
-        }
-    }
+async fn call_localhost(client: Client) -> Result<()> {
+    let response = client.get("https://localhost:8080").send().await?;
+    let response = response.error_for_status()?;
+    response.bytes().await?;
+    Ok(())
 }
